13e84a44a0e46062875a5413303f99bf3961967a
Terri J <terbr@hotmail.com>
2020-05-16 17:20:54 -0700

    added function get number, upper, lower, special

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+// Generator funtion
+//get lower case  
+function getlower(){
+  return String.fromCharCode(Math.floor(Math.random() *26)+ 97);
+} 
+console.log(String.fromCharCode(Math.floor(Math.random() *26)+ 97)) 
+
+function getupper(){
+  return String.fromCharCode(Math.floor(Math.random() *26)+ 65);
+} 
+console.log(String.fromCharCode(Math.floor(Math.random() *26)+ 65)) 
+
+function getspecialchar(){
+  return String.fromCharCode(Math.floor(Math.random() *10)+ 33);
+} 
+console.log(String.fromCharCode(Math.floor(Math.random() *10)+ 33)) 
+
+function getnumber(){
+  return String.fromCharCode(Math.floor(Math.random() *10)+ 48);
+} 
+console.log(String.fromCharCode(Math.floor(Math.random() *10)+ 48)) 
+
+
+
+
+
 // Write password to the #password input
-function writePassword() {
+/*function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +40,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
