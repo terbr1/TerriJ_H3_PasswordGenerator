d0c84e1431a2437341bb21718968d8f0a1521d14
Terri J <terbr@hotmail.com>
2020-05-16 21:02:37 -0700

    added if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,67 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Prompts and Confirms
+var passLength = prompt("How many character do you want your password to be? Enter a number between 8 and 20.")
+
+var passLower = confirm("Would you like to include lower case letters in your password?")
+
+var passUpper = confirm("Would you like to include upper case letters in your password?")
+
+var passSpecial = confirm("Would you like special characters in your password?") 
+
+var passNumber = confirm("Would you like a number your password?")
+ 
+var generateBtn = document.getElementById("#generate")
+
+ // generateBtn.onclick = function(){
+// Generator funtion 
+const passvalues = {
+  lower: getlower,
+  upper: getupper,
+  specialchar: getspecialchar,
+  number: getnumber
+}
 
 
-
-// Generator funtion
-//get lower case  
+//https://www.w3schools.com/html/html_charset.asp
+//get lower case  used 
 function getlower(){
   return String.fromCharCode(Math.floor(Math.random() *26)+ 97);
 } 
-console.log(String.fromCharCode(Math.floor(Math.random() *26)+ 97)) 
-
 function getupper(){
   return String.fromCharCode(Math.floor(Math.random() *26)+ 65);
 } 
-console.log(String.fromCharCode(Math.floor(Math.random() *26)+ 65)) 
-
 function getspecialchar(){
   return String.fromCharCode(Math.floor(Math.random() *10)+ 33);
 } 
-console.log(String.fromCharCode(Math.floor(Math.random() *10)+ 33)) 
-
 function getnumber(){
   return String.fromCharCode(Math.floor(Math.random() *10)+ 48);
 } 
-console.log(String.fromCharCode(Math.floor(Math.random() *10)+ 48)) 
 
 
+if(passLength >= 8 && passLength <= 128){
+  passLower
+  console.log(passLength)
+} else {passLength === false
+console.log(passLength)};
 
+if(passLower === true){
+  passUpper
+} else {passLower ===false
+console.log(passLower)}
 
+if(passUpper === true){
+  passSpecial
+} else {passUpper ===false
+console.log(passUpper)}
+
+if(passSpecial === true){
+  passNumber
+} else {passSpecial ===false
+console.log(passSpecial)}
+
+if(passNumber === true){
+  passNumber
+} else {passSpecial ===false
+console.log(passSpecial)}
 
 // Write password to the #password input
 /*function writePassword() {
@@ -37,7 +71,11 @@ console.log(String.fromCharCode(Math.floor(Math.random() *10)+ 48))
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 */
+// Add event listener to generate button
+generateBtn.addEventListener("click",
+
+
+
+
+//writePassword);
