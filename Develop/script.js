9b23694008157d99f1ad65a8833e902220bf7bdf
Terri J <terbr@hotmail.com>
2020-05-16 22:00:44 -0700

    added button click

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,11 @@
-// Prompts and Confirms
+
+
+var generateBtn = document.querySelector("#generate")
+generateBtn.onclick = function(){
+
+
+// Prompts and confirms
+
 var passLength = prompt("How many character do you want your password to be? Enter a number between 8 and 20.")
 
 var passLower = confirm("Would you like to include lower case letters in your password?")
@@ -8,21 +15,18 @@ var passUpper = confirm("Would you like to include upper case letters in your pa
 var passSpecial = confirm("Would you like special characters in your password?") 
 
 var passNumber = confirm("Would you like a number your password?")
- 
-var generateBtn = document.getElementById("#generate")
 
- // generateBtn.onclick = function(){
+
+
+
 // Generator funtion 
-const passvalues = {
-  lower: getlower,
-  upper: getupper,
-  specialchar: getspecialchar,
-  number: getnumber
-}
+
 
 
 //https://www.w3schools.com/html/html_charset.asp
 //get lower case  used 
+
+
 function getlower(){
   return String.fromCharCode(Math.floor(Math.random() *26)+ 97);
 } 
@@ -63,19 +67,16 @@ if(passNumber === true){
 } else {passSpecial ===false
 console.log(passSpecial)}
 
+};
+
 // Write password to the #password input
-/*function writePassword() {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-*/
+
 // Add event listener to generate button
-generateBtn.addEventListener("click",
-
-
-
-
-//writePassword);
+generateBtn.addEventListener("click", writePassword);
